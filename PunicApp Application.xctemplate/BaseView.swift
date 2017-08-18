682ef1f28a3c5a5f0be530ed8c6b326071bcff7d
Denis Moskalenko <dmoskalenko@punicapp.com>
2017-08-18 14:41:50 +0700

    add BaseView

---
+++ PunicApp Application.xctemplate/BaseView.swift
@@ -0,0 +1,21 @@
+//
+//  BaseView.swift
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
