9964727d43c4be2dd19d6343d8a6b4f3ec49b653
Denis Moskalenko <dmoskalenko@punicapp.com>
2017-08-24 15:40:07 +0700

    fix swift project view controller for test

---
+++ PunicApp Swift App.xctemplate/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: BaseViewController {
+    
+    //MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+     override func viewWillAppear(_ animated: Bool) {
+     super.viewWillAppear(animated)
+     }
+     
+     override func viewDidAppear(_ animated: Bool) {
+     super.viewDidAppear(animated)
+     }
+     
+     override func viewWillDisappear(_ animated: Bool) {
+     super.viewWillDisappear(animated)
+     }
+     
+     override func viewDidDisappear(_ animated: Bool) {
+     super.viewDidDisappear(animated)
+     }
+     
+     override func didReceiveMemoryWarning() {
+     super.didReceiveMemoryWarning()
+     // Dispose of any resources that can be recreated.
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
