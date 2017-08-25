27070ef2e17352f401611159b8756a777973ac7c
Denis Moskalenko <dmoskalenko@punicapp.com>
2017-08-25 12:41:17 +0700

    correct swift view controller template

---
+++ FileTemplates/PunicApp File Templates/View Controller(Swift).xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ___FILEBASENAMEASIDENTIFIER___ViewController.swift
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___ViewController: BaseViewController {
+    
+    // MARK: - View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___ViewController {
+    
+}
