a1ccce04613259818fc64525b46097ef7bdfb9fd
Denis Moskalenko <dmoskalenko@punicapp.com>
2017-08-25 14:46:07 +0700

    add objective C file template

---
+++ FileTemplates/PunicApp File Templates/View Controller(ObjC).xctemplate/___FILEBASENAME___ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ___FILEBASENAMEASIDENTIFIER___ViewController.swift
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ViewController: BaseViewController
+
+
+@end
+
---
+++ FileTemplates/PunicApp File Templates/View Controller(ObjC).xctemplate/___FILEBASENAME___ViewController.m
@@ -0,0 +1,47 @@
+//
+//  ___FILEBASENAMEASIDENTIFIER___ViewController.swift
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+#pragma mark - View Life Cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+/*
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+*/
+
+@end
