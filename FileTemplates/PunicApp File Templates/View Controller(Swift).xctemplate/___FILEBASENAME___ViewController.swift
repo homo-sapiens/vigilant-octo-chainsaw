5b80c2397d61523326f6390f2fc75ba324dea574
Denis Moskalenko <dmoskalenko@punicapp.com>
2017-08-26 17:35:39 +0700

    correct view controller swift file template

--- FileTemplates/PunicApp File Templates/View Controller(Swift).xctemplate/___FILEBASENAME___ViewController.swift
+++ FileTemplates/PunicApp File Templates/View Controller(Swift).xctemplate/___FILEBASENAME___ViewController.swift
@@ -14,6 +14,39 @@ class ___FILEBASENAMEASIDENTIFIER___ViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    /*
+     override func viewWillAppear(_ animated: Bool) {
+     super.viewWillAppear(animated)
+     }
+     
+     override func viewDidAppear(_ animated: Bool) {
+     super.viewDidAppear(animated)
+     }
+     
+     override func viewWillDisappear(_ animated: Bool) {
+     super.viewWillDisappear(animated)
+     }
+     
+     override func viewDidDisappear(_ animated: Bool) {
+     super.viewDidDisappear(animated)
+     }
+     
+     override func didReceiveMemoryWarning() {
+     super.didReceiveMemoryWarning()
+     // Dispose of any resources that can be recreated.
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
 
 extension ___FILEBASENAMEASIDENTIFIER___ViewController {
